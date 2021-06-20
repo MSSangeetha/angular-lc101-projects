5a1759d000aaf34672812ccbb32b1b4229ee846f
MSSangeetha <sangeethasanthosh.m@gmail.com>
2021-06-20 15:17:19 -0500

    First commit for excercises

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Random Images';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8I4ACnYAUxGLMbBA0xklei1OF4PIPuAAC9g&usqp=CAU';
+  image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe0O0260hzKyKursZUTtZAxECP0gSVJ2JXwQ&usqp=CAU';
 
   constructor() { }
 
