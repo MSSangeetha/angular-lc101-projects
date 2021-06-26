2b59bd94e4072f718fb42eddba53bbe5543a889b
MSSangeetha <sangeethasanthosh.m@gmail.com>
2021-06-26 12:48:56 -0500

    excercises angular part 2

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -24,9 +24,15 @@ export class CandidatesComponent implements OnInit {
   }
 
   // Code the addToCrew function here:
-
-
+  addToCrew(person : object) {
+if(!this.crew.includes(person)){
+  this.crew.push(person);
+}
+  }
   // BONUS: Code the changeMissionName function here:
-
+  changeMissionName(inputName : string) {
+    this.missionName = inputName;
+    return false;
+  }
 
 }
